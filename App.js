e7554e76f746876f47ee3002bd10b93b1912e184
Vince Linise <vincelinise@gmail.com>
2024-01-04 17:04:12 +0100

    Added native-like components

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StatusBar, Text, useColorScheme } from 'react-native';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import UITheme from './props/NativeColors';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={require('./views/HomeScreen').default} />
+      <Stack.Screen name="Details" component={require('./views/DetailsScreen').default} />
+    </Stack.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const scheme = useColorScheme();
+
+  return (
+    <NavigationContainer theme={UITheme(scheme)}>
+      <StatusBar barStyle={scheme === 'dark' ? 'light-content' : 'dark-content'} />
+      <Navigation />
+    </NavigationContainer>
+  );
+}
